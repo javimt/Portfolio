c581371fe7350f5fc431638ff3829e6a7d1dcf92
javimt <javiergp_19@outlook.com>
2023-07-05 16:52:56 -0500

    working on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,24 @@
 import styles from "../styles/NavBar.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react"
+import { GiHamburgerMenu, GiCrossedBones } from 'react-icons/gi';
+import image from "../assets/mi logo1.png"
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <Link to="/">
-          <img src="ruta_del_logo.png" alt="Logo" />
+          <img src={image} alt="Logo" />
         </Link>
       </div>
-      <div className={styles.links}>
+      <div className={`${styles.links} ${showMenu ? styles.show : ''}`}>
         <Link to="/about" className={styles.link}>
           About
         </Link>
@@ -19,6 +28,9 @@ const NavBar = () => {
         <Link to="/home" className={styles.link}>
           Home
         </Link>
+      </div>
+      <div className={styles.menuIcon} onClick={handleMenuClick}>
+        {showMenu ? <GiCrossedBones color="#E6B17E" /> : <GiHamburgerMenu color="#E6B17E" />}
       </div>
     </nav>
   );
