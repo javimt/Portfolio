c477e985152e21d70574d8b96fe0f1733db194ee
javimt <javiergp_19@outlook.com>
2023-07-20 21:28:32 -0500

    working on Spheres

--- client/src/components/Spheres.jsx
+++ client/src/components/Spheres.jsx
@@ -6,16 +6,28 @@ const Spheres = ({ position, size, children }) => {
   const meshRef = useRef();
   const { size: canvasSize } = useThree();
 
-  const adjustedSize = (size * canvasSize.width) / 1000;
-  const adjustedPosition = position.map((p) => (p * canvasSize.width) / 1000);
-  const iconSize = canvasSize.width * 0.1;
+  //let adjustedSize = (size * canvasSize.width) / 1000;
+  //let adjustedPosition = position.map((p) => (p * canvasSize.width) / 1000);
+
+  //let windowSize = window.innerWidth < 1000 ? window.innerWidth : 1000;
+
+  const minDimension = Math.max(canvasSize.width, canvasSize.height);
+  let adjustedSize = (size * minDimension) / 1000;
+  let adjustedPosition = position.map((p) => (p * minDimension) / 1000);
+
+  if (canvasSize.width <= 1000) {
+    adjustedSize = (size * minDimension) / 1000;
+    adjustedPosition = position.map((p) => (p * minDimension) / 1000);
+  } 
+
+  const iconSize = minDimension * 0.1;
 
   useFrame(() => (meshRef.current.rotation.y += 0.01));
 
   return (
     <mesh ref={meshRef} position={adjustedPosition}>
       <sphereGeometry args={[adjustedSize, 12, 12]} />
-      <meshBasicMaterial color="#C56E33" wireframe />
+      <meshBasicMaterial color="#C56E33" wireframe wireframeLinecap="0.1" wireframeLinewidth={0.1}/>
       <Html
         center
         style={{
--- client/src/pages/Skills3d.jsx
+++ client/src/pages/Skills3d.jsx
@@ -17,102 +17,106 @@ import styles from "../styles/Skills3d.module.css";
 import Spheres from "../components/Spheres";
 
 const Skills3d = () => {
-  const [windowSize, setWindowSize] = useState({width: window.innerWidth, height: window.innerHeight});
-  const cameraRef = useRef();
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  //const cameraRef = useRef();
 
   useEffect(() => {
-    
     const handleResize = () => {
-      if (cameraRef.current) {
+      /* if (cameraRef.current) {
       const aspect = window.innerWidth / window.innerHeight;
       const zoom = window.innerWidth < 1000 ? 100 : 50;
       cameraRef.current.aspect = aspect;
       cameraRef.current.zoom = zoom;
-      cameraRef.current.updateProjectionMatrix();
-      setWindowSize(window.innerWidth);
-      }
+      cameraRef.current.updateProjectionMatrix(); */
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+      //}
     };
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const iconSize = windowSize.width * 0.1;
+  //const iconSize = windowSize.width * 0.1;
 
-  const spheres = useMemo(() => [
-    {
-      id: "1",
-      icon: <FaNodeJs size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [0, 0, 0],
-    },
-    {
-      id: "2",
-      icon: <BiLogoPostgresql size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [7, 0, 0],
-    },
-    {
-      id: "3",
-      icon: <TbBrandJavascript size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [-7, 0, 0],
-    },
-    {
-      id: "4",
-      icon: <FaReact size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [3.5, 0, 0],
-    },
-    {
-      id: "5",
-      icon: <FaCss3Alt size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [-3.5, 0, 0],
-    },
-    {
-      id: "6",
-      icon: <FaDatabase size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [-7, 3.5, 0],
-    },
-    {
-      id: "7",
-      icon: <FaHtml5 size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [3.5, 3.5, 0],
-    },
-    {
-      id: "8",
-      icon: <SiRedux size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [0, 3.5, 0],
-    },
-    {
-      id: "9",
-      icon: <BsGit size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [-3.5, 3.5, 0],
-    },
-    {
-      id: "10",
-      icon: <FaGithubSquare size={iconSize} color="rgb(42, 94, 94)" />,
-      position: [7, 3.5, 0],
-    },
-  ], [windowSize])
+  const spheres = useMemo(
+    () => [
+      {
+        id: "1",
+        icon: <FaNodeJs color="rgb(42, 94, 94)" />,
+        position: [0, 0, 0],
+      },
+      {
+        id: "2",
+        icon: <BiLogoPostgresql color="rgb(42, 94, 94)" />,
+        position: [7, 0, 0],
+      },
+      {
+        id: "3",
+        icon: <TbBrandJavascript color="rgb(42, 94, 94)" />,
+        position: [-7, 0, 0],
+      },
+      {
+        id: "4",
+        icon: <FaReact color="rgb(42, 94, 94)" />,
+        position: [3.5, 0, 0],
+      },
+      {
+        id: "5",
+        icon: <FaCss3Alt color="rgb(42, 94, 94)" />,
+        position: [-3.5, 0, 0],
+      },
+      {
+        id: "6",
+        icon: <FaDatabase color="rgb(42, 94, 94)" />,
+        position: [-7, 3.5, 0],
+      },
+      {
+        id: "7",
+        icon: <FaHtml5 color="rgb(42, 94, 94)" />,
+        position: [3.5, 3.5, 0],
+      },
+      {
+        id: "8",
+        icon: <SiRedux color="rgb(42, 94, 94)" />,
+        position: [0, 3.5, 0],
+      },
+      {
+        id: "9",
+        icon: <BsGit color="rgb(42, 94, 94)" />,
+        position: [-3.5, 3.5, 0],
+      },
+      {
+        id: "10",
+        icon: <FaGithubSquare  color="rgb(42, 94, 94)" />,
+        position: [7, 3.5, 0],
+      },
+    ],
+    [windowSize]
+  );
 
   const adjustedSpheres = spheres.map((sphere, index) => {
     const adjustedPosition = [...sphere.position];
+    const minWindowSize = Math.min(windowSize.width, windowSize.height);
     let adjustedSize;
+
     // Calculamos el espacio entre las esferas en función del tamaño de la ventana.
-    const spacing = 3
-      //windowSize <= 1000 ? setWindowSize / (spheres.length / 2) : 3;
-    
-      if (windowSize <= 1000) {
-console.log("putas esferas")
-      adjustedSize = windowSize / 100;
+    if (windowSize.width <= 1000 & windowSize.height > 200) {
+      console.log("putas esferas");
+      adjustedSize = windowSize.height / 500;
+
       // Calculamos la posición en el eje X e Y en función del índice de la esfera.
-      adjustedPosition[0] =
-        (index % 2) * spacing - windowSize / 2 + spacing / 2;
-      adjustedPosition[1] =
-        Math.floor(index / 2) * spacing - windowSize / 2 + spacing / 2;
+      adjustedPosition[0] = (index % 2 === 0 ? -5.5 : 5.5) * (minWindowSize / 1000); //index % 2 === 0 ? - 5 : 5; // Esto divide las esferas en dos columnas
+      adjustedPosition[1] = (Math.floor(index / 2) * 7.9 - 18) * (minWindowSize / 1000); //Math.floor(index / 2) * 6 - 15;
     } else {
       adjustedSize = 1.5;
       // Si la ventana es más grande, las esferas se colocan en una cuadrícula de 2x5.
-      adjustedPosition[0] = (index % 5) * 3.2 - 6.5;
-      adjustedPosition[1] = Math.floor(index / 5) * 4 - 2;
+      adjustedPosition[0] = (index % 5) * 3.3 - 7.08;
+      adjustedPosition[1] = Math.floor(index / 5) * 4 - 2.8;
     }
+
     return { ...sphere, position: adjustedPosition, size: adjustedSize };
   });
 
@@ -133,7 +137,11 @@ console.log("putas esferas")
         />
 
         {adjustedSpheres.map((sphere) => (
-          <Spheres key={sphere.id} position={sphere.position} size={sphere.size}>
+          <Spheres
+            key={sphere.id}
+            position={sphere.position}
+            size={sphere.size}
+          >
             {sphere.icon}
           </Spheres>
         ))}
