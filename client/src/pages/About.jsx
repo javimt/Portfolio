b92149cc45c234eb42815ef89f3b2f8c73905670
javimt <javiergp_19@outlook.com>
2023-10-18 13:46:00 -0500

    some change on about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -12,12 +12,10 @@ const About = () => {
         vendiendo dispositivos mobiles, tablets y accesorios.
         Despues de esto emprendí con dos almacenes de tecnología donde daba asesoria a mis clientes,
         reparaba y vendía dispositivos móbiles y tablets, accesorios y mas, durante 9 años. Asi que tengo amplia experiencia
-        en atencion al cliente, generando estrategias de ventas e incorporando neuromercadeo y un poco de PNL.
+        en atencion al cliente y generando estrategias de ventas.
         Luego me empecé a inclinar por el desarrollo de software y ahora soy Full Stack Web Developer, mundo en el cual
-        estoy completamente inmerso en este momento, y que cada vez me apasiona más, pues soy un loco empedernido por los
-        desafios y el conocimiento. Soy muy disciplinado, enfocado, con un caracter fuerte y con una mentalidad emprendedora. Siempre busco crecer
-        en todos los ambitos de mi vida (conocimiento, fuerza y espiritu) y que tambien lo hagan las personas que me rodean.
-
+        estoy completamente inmerso en este momento, y que cada vez me apasiona más. Soy muy disciplinado, enfocado, con un caracter fuerte y con una mentalidad
+        emprendedora. Siempre busco crecer en todos los ambitos de mi vida y que tambien lo hagan las personas que me rodean.
       </p>
     </div>
   )
